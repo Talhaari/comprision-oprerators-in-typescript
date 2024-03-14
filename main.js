944fbe9b2405a3d87f7f1ddecd41494bb57f65cd
Talha arif <160534871+Talhaari@users.noreply.github.com>
2024-03-14 22:44:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Comparision Opreators in typescript
+var a = 5;
+var b = 2;
+var equal = (a == b); //false
+console.log(equal);
+var isnotequal = (a != b); //true
+console.log(isnotequal);
+var greaterthen = (a > b); //true
+console.log(greaterthen);
+var lowerthen = (a < b); //false
+console.log(lowerthen);
---
+++ main.ts
@@ -0,0 +1,11 @@
+// Comparision Opreators in typescript
+let a = 5
+let b = 2
+let equal = (a == b)    //false
+console.log(equal)
+let isnotequal = (a != b) //true
+console.log(isnotequal)
+let greaterthen = (a > b) //true
+console.log(greaterthen)
+let lowerthen = (a < b) //false
+console.log(lowerthen)
